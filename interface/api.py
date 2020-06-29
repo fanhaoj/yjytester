57a75ac3a3cf2426e06f450f0e1afc9506f4e9b0
hao.fan <286305546@qq.com>
2020-06-29 22:34:08 +0800

    提交

--- interface/api.py
+++ interface/api.py
@@ -1,28 +1,52 @@
+#coding=utf-8
 import json
 
+import pytest
 import requests
+import  time
 
+# class Test_Interyjy():
+#     # def setup(self):
+#     global date,list
+#     date=time.strftime("%Y-%m-%d",time.localtime())
+#     list={}
+#     print(list)
 
-class Login:
-    def gyLogin(self):
-        url = "http://yjy.zhiyousx.com:8765/api/auth/jwt/token"
-        data = {"username": "ziyuan", "password": "123456", "platformType": "NORMAL"}
-        headers = {"Content-Type": "application/json"}
-        r = requests.post(url, data=json.dumps(data), headers=headers)
-        gytoken=r.json()['data']
-        return gytoken
-        assert r.status_code == 200
+#下单
+@pytest.fixture(autouse=True)
+def buyprocedure(self,fxLogin):
+    url = "http://yjy.zhiyousx.com:8765//api/order/order/ticket/create"
+    data = {"departDate":"2020-06-29","productId":"1317","productNum":"1","touristInfo":[{"name":"测试","mobile":"15009253686"}]}
+    headers = {"Authorization":fxLogin,"Content-Type": "application/json"}
+    r=requests.post(url,data=json.dumps(data),headers=headers)
+    orderid = r.json()['data']['id']
+    print(orderid)
+    return  orderid
+    assert r.status_code == 200
 
+#支付
+@pytest.fixture()
+def payprocedure(self,fxLogin,test_buyprocedure):
+    url = "http://yjy.zhiyousx.com:8765//api/order/order/ticket/pay/credit?orderId="+str(test_buyprocedure)
+    data = {}
+    headers = {"Authorization":fxLogin,"Content-Type": "application/json"}
+    r=requests.post(url,data=json.dumps(data),headers=headers)
+    assert  r.status_code==200
 
-    def fxLogin(self):
-        url = "http://yjy.zhiyousx.com:8765/api/auth/jwt/token"
-        data = {"username": "haofanfx", "password": "123456", "platformType": "NORMAL"}
-        headers = {"Content-Type": "application/json"}
-        r = requests.post(url, data=json.dumps(data), headers=headers)
-        fxtoken=r.json()['data']
-        return fxtoken
-        assert r.status_code == 200
+#退款
+def refund(self,fxLogin,test_buyprocedure):
+    url = "http://yjy.zhiyousx.com:8765/api/order/order/ticket/refund"
+    data = {"orderId":test_buyprocedure,"reason":"拍错退款","refundNum":1}
+    headers = {"Authorization":fxLogin,"Content-Type": "application/json"}
+    r=requests.post(url,data=json.dumps(data),headers=headers)
+    print(r.text)
+    assert r.status_code==200
 
-if __name__ == '__main__':
-    l=Login()
-    l.fxLogin()
+#取消订单
+def cancel(self,fxLogin,test_buyprocedure):
+    url = "http://yjy.zhiyousx.com:8765/api/order/order/ticket/cancel"
+    data = {"orderSn": test_buyprocedure}
+    headers = {"Authorization": fxLogin, "Content-Type": "application/x-www-form-urlencoded"}
+    r = requests.post(url, data, headers)
+    print(r.text)
+    assert r.status_code==200
--- interface/conftest.py
+++ interface/conftest.py
@@ -17,9 +17,7 @@ def gyLogin():
     data = {"username": "ziyuan", "password": "123456", "platformType": "NORMAL"}
     headers = {"Content-Type": "application/json"}
     r = requests.post(url, data=json.dumps(data), headers=headers)
-    result = r.text
-    jsr = json.loads(result)
-    gytoken = jsr['data']
+    gytoken = r.json()['data']
     yield gytoken
     assert r.status_code == 200
 
@@ -30,9 +28,30 @@ def fxLogin():
     data = {"username": "haofanfx", "password": "123456", "platformType": "NORMAL"}
     headers = {"Content-Type": "application/json"}
     r = requests.post(url, data=json.dumps(data), headers=headers)
-    result = r.text
-    print(result)
-    jsr = json.loads(result)
-    fxtoken = jsr['data']
-    assert r.status_code == 200
+    fxtoken = r.json()["data"]
     yield fxtoken
+    assert r.status_code == 200
+
+#下单
+@pytest.fixture(autouse=True)
+def buyprocedure(fxLogin):
+    url = "http://yjy.zhiyousx.com:8765//api/order/order/ticket/create"
+    data = {"departDate":"2020-06-29","productId":"1878","productNum":"1","touristInfo":[{"name":"测试","mobile":"15009253686"}]}
+    headers = {"Authorization":fxLogin,"Content-Type": "application/json"}
+    r=requests.post(url,data=json.dumps(data),headers=headers)
+    orderid = r.json()['data']['id']
+    print(orderid)
+    return  orderid
+    assert r.status_code == 200
+
+#支付
+@pytest.fixture(autouse=True)
+def payprocedure(self,fxLogin,buyprocedure):
+    url = "http://yjy.zhiyousx.com:8765//api/order/order/ticket/pay/credit?orderId="+str(buyprocedure)
+    data = {}
+    headers = {"Authorization":fxLogin,"Content-Type": "application/json"}
+    r=requests.post(url,data=json.dumps(data),headers=headers)
+    assert  r.status_code==200
+
+if __name__ == '__main__':
+    fxLogin()
